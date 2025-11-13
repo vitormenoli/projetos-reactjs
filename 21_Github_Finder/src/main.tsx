24551bd04b4c9c408e4f2b8016e5ab5051be9230
Vitor Menoli <vitoragario@gmail.com>
2025-11-13 03:00:06 -0300

    fix: fixing a router error

--- 21_Github_Finder/src/main.tsx
+++ 21_Github_Finder/src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home.tsx'
 import Repos from './routes/Repos.tsx'
 import Favorites from './routes/Favorites'
 import { ThemeProvider } from './context/ThemeContext'
 import { FavoritesProvider } from './context/FavoritesContext'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
